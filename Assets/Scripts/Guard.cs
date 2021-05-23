581c1a319b96bd12b952a9a0eb9d0324a6853fd1
Shadow090 <65199168+Shadow090@users.noreply.github.com>
2021-05-22 20:32:50 -0700

    Initial commit

---
+++ Assets/Scripts/Guard.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Guard : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject losePanel;
+    public bool alerted;
+    public float speed = 10f;
+
+    private NavMeshAgent navMesh;
+    private Vector3 home;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        navMesh.speed = speed;
+
+        home = transform.position;
+        alerted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alerted)
+        {
+            navMesh.destination = player.transform.position;
+        }
+
+        else
+        {
+            navMesh.destination = home;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player") && alerted)
+        {
+            losePanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+    }
+}
+
---
+++ JailEscape/JailEscape/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace JailEscape
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
